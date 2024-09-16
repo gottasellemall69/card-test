475484d7f29105e68362fffd9569f56d1063f1d6
gottasellemall69 <gottasellemall69@gmail.com>
2024-09-16 05:47:22 -0700

    Update next.config.js
    Signed-off-by: gottasellemall69 <gottasellemall69@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
+  const nextConfig = {
   reactStrictMode: true,
+};
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://infinite-api.tcgplayer.com https://card-test-ashy.vercel.app https://va.vercel-scripts.com https://db.ygoprodeck.com https://sportscardspro.com;
+              style-src 'self' 'unsafe-inline' https://card-test-ashy.vercel.app;
+              img-src 'self' data:;
+              object-src 'none';
+              connect-src 'self' https://infinite-api.tcgplayer.com https://db.ygoprodeck.com;
+              font-src 'self';
+              frame-ancestors 'none';
+              upgrade-insecure-requests;
+              block-all-mixed-content;
+            `.trim(),
+          },
+        ],
+      },
+    ];
+  },
 };
