a29bbd468e96e46d17d499338130f8f72b584814
gottasellemall69 <gottasellemall69@gmail.com>
2024-09-16 06:23:23 -0700

    Update next.config.js
    Signed-off-by: gottasellemall69 <gottasellemall69@gmail.com>

--- next.config.js
+++ next.config.js
@@ -2,27 +2,3 @@
 const nextConfig = {
   reactStrictMode: true,
 };
-
- // next.config.js
-module.exports = {
-  async headers() {
-    return [
-      {
-        source: '/(.*)', // Applies to all routes
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: `
-              default-src 'self' db.ygoprodeck.com;
-              script-src 'self' 'unsafe-inline' 'unsafe-eval' infinite-api.tcgplayer.com card-test-ashy.vercel.app va.vercel-scripts.com db.ygoprodeck.com sportscardspro.com;
-              style-src 'self' 'unsafe-inline' card-test-ashy.vercel.app;
-              img-src 'self' data:;
-              object-src 'none';
-            `.trim(), // Remove extra whitespace
-          },
-        ],
-      },
-    ];
-  },
-};
-
