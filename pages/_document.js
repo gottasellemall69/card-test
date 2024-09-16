024ca1928c449e1fbeece3afa1aee68282735b4c
gottasellemall69 <gottasellemall69@gmail.com>
2024-09-16 06:23:48 -0700

    Update _document.js
    Signed-off-by: gottasellemall69 <gottasellemall69@gmail.com>

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,16 @@ export default function Document() {
         <meta name="description" content="Enter list of TCG cards, get data back" />
         <meta name="keywords" content="javascript,nextjs,price-tracker,trading-card-game,tailwindcss" />
         <meta name="charSet" content="UTF-8" />
-      
+      <meta
+          httpEquiv="Content-Security-Policy"
+          content="
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' infinite-api.tcgplayer.com card-test-ashy.vercel.app va.vercel-scripts.com db.ygoprodeck.com sportscardspro.com;
+              style-src 'self' 'unsafe-inline' card-test-ashy.vercel.app;
+              img-src 'self' data:;
+              object-src 'none';
+              default-src 'self' db.ygoprodeck.com;
+            "
+        />
       </Head>
       <body className="h-full w-full">
         <div className="w-full transition-all main">
