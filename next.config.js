2341a0648bc249320fce47940f6b4a934b29b723
gottasellemall69 <gottasellemall69@gmail.com>
2024-09-16 06:08:10 -0700

    Update next.config.js
    Signed-off-by: gottasellemall69 <gottasellemall69@gmail.com>

--- next.config.js
+++ next.config.js
@@ -3,29 +3,26 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-  module.exports = {
+ // next.config.js
+module.exports = {
   async headers() {
     return [
       {
-        source: '/(.*)',
+        source: '/(.*)', // Applies to all routes
         headers: [
           {
             key: 'Content-Security-Policy',
             value: `
-              default-src 'self' 'unsafe-inline';
-              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://infinite-api.tcgplayer.com https://card-test-ashy.vercel.app https://va.vercel-scripts.com https://db.ygoprodeck.com https://sportscardspro.com;
-              style-src 'self' 'unsafe-inline' https://card-test-ashy.vercel.app;
+              default-src 'self' db.ygoprodeck.com;
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' infinite-api.tcgplayer.com card-test-ashy.vercel.app va.vercel-scripts.com db.ygoprodeck.com sportscardspro.com;
+              style-src 'self' 'unsafe-inline' card-test-ashy.vercel.app;
               img-src 'self' data:;
               object-src 'none';
-              connect-src 'self' https://infinite-api.tcgplayer.com https://db.ygoprodeck.com;
-              font-src 'self';
-              frame-ancestors 'none';
-              upgrade-insecure-requests;
-              block-all-mixed-content;
-            `.trim(),
+            `.trim(), // Remove extra whitespace
           },
         ],
       },
     ];
   },
 };
+
